9987b51b70c3dbec0fad8a9004560d223c54e843
marc hoffman <mh@remobjects.com>
2014-07-01 19:03:10 +0200

    First very early commit

---
+++ Source/Aliases.swift
@@ -0,0 +1,15 @@
+﻿
+typealias Int = Int32
+typealias Int8 = SByte
+
+typealias UInt = UInt32
+typealias UInt8 = Byte
+
+typealias Bool = Boolean
+
+typealias UnicodeScalar = Char // for now
+typealias FloatingPointClassification = Int // hack for now
+
+typealias Float = Single
+
+//typealias Any = Dynamic
---
+++ Source/Properties/Settings.Designer.cs
@@ -0,0 +1,7 @@
+namespace Swift.Echoes
+{
+
+/* The .Designer.cs file will be automatically generated when the parent file changes.
+  You can also right-click the parent file and select 'Run Custom Tool' to update it now. */
+
+}
