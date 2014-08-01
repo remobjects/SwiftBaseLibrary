cbaf0dbd095bc92bdcbfc61cfa7c8ce36cf629d9
marc hoffman <mh@remobjects.com>
2014-08-01 18:00:32 +0200

    Commandline Parameter (C_ARGC/C_ARGV) support

---
+++ Source/Commandline.swift
@@ -0,0 +1,25 @@
+﻿var __C_ARGC = 0;
+var __C_ARGV = String[](0);
+public var C_ARGC: Int { get { return __C_ARGC; } }
+public var C_ARGV: String[] { get { return __C_ARGV; } }
+
+public func `@@nougatSetArgV`(args: String[]) {
+	__C_ARGC = length(args);
+	__C_ARGV = args;
+}
+
+#if NOUGAT
+public func __stringArrayToCStringArray(arcv: String[]) -> (UnsafePointer<AnsiChar>)[] {
+	
+	var result = UnsafePointer<AnsiChar>[](length(arcv))
+	for var i = 0; i < arcv.length; i++ {
+		if arcv[i] != nil {
+			result[i] = arcv[i].UTF8String
+		} else {
+			result[i] = nil
+		}
+		//result[i] = arcv[i] != nil ? arcv[i].UTF8String : ""
+	}
+	return result
+}
+#endif
