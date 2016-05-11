f7bf87eec9caa00dd0d0a09d2dcfa337cc627e65
marc hoffman <mh@remobjects.com>
2016-05-11 09:27:57 -0400

    Tab vs space fixes

--- Source/Index_Protocols.swift
+++ Source/Index_Protocols.swift
@@ -1,44 +1,44 @@
 ﻿
 public protocol ForwardIndexType {
-    associatedtype Distance /*: SignedIntegerType*/ = Int
-    @warn_unused_result func advancedBy(n: Self.Distance) -> Self
-    @warn_unused_result func advancedBy(n: Self.Distance, limit: Self) -> Self
-    @warn_unused_result func distanceTo(end: Self) -> Self.Distance
+	associatedtype Distance /*: SignedIntegerType*/ = Int
+	@warn_unused_result func advancedBy(n: Self.Distance) -> Self
+	@warn_unused_result func advancedBy(n: Self.Distance, limit: Self) -> Self
+	@warn_unused_result func distanceTo(end: Self) -> Self.Distance
 }
 
 public protocol BidirectionalIndexType : ForwardIndexType {
-    //@warn_unused_result func advancedBy(_ n: Self.Distance) -> Self                    // duped from ForwardIndexType?
-    //@warn_unused_result func advancedBy(_ n: Self.Distance, limit limit: Self) -> Self // duped from ForwardIndexType?
-    @warn_unused_result func predecessor() -> Self
-    @warn_unused_result func successor() -> Self
+	//@warn_unused_result func advancedBy(_ n: Self.Distance) -> Self					// duped from ForwardIndexType?
+	//@warn_unused_result func advancedBy(_ n: Self.Distance, limit limit: Self) -> Self // duped from ForwardIndexType?
+	@warn_unused_result func predecessor() -> Self
+	@warn_unused_result func successor() -> Self
 }
 
 public protocol ReverseIndexType : BidirectionalIndexType {
-    associatedtype Base : BidirectionalIndexType
-    //associatedtype Distance : SignedIntegerType = Self.Base.Distance // duped from ForwardIndexType?
-    init(_ base: Self.Base)
-    var base: Self.Base { get }
+	associatedtype Base : BidirectionalIndexType
+	//associatedtype Distance : SignedIntegerType = Self.Base.Distance // duped from ForwardIndexType?
+	init(_ base: Self.Base)
+	var base: Self.Base { get }
 }
 
 public protocol Strideable : Comparable {
-    associatedtype Stride : SignedNumberType
-    //74968: Silver: compiler ignores undefined associated type (`Self.whatever`)
-    
-    @warn_unused_result func advancedBy(n: Self.Stride) -> Self
-    @warn_unused_result func distanceTo(other: Self) -> Self.Stride
-    @warn_unused_result func stride(# through: Self, by: Self) -> ISequence<Self>
-    //74693: Silver: can't overload interface methods by secondary names
-    //func stride(# to: Self, by: Self) -> ISequence<Self>
+	associatedtype Stride : SignedNumberType
+	//74968: Silver: compiler ignores undefined associated type (`Self.whatever`)
+	
+	@warn_unused_result func advancedBy(n: Self.Stride) -> Self
+	@warn_unused_result func distanceTo(other: Self) -> Self.Stride
+	@warn_unused_result func stride(# through: Self, by: Self) -> ISequence<Self>
+	//74693: Silver: can't overload interface methods by secondary names
+	//func stride(# to: Self, by: Self) -> ISequence<Self>
 }
 
 public protocol RandomAccessIndexType : BidirectionalIndexType, Strideable {
-    //@warn_unused_result func advancedBy(_ n: Self.Distance) -> Self                    // duped from ForwardIndexType?
-    //@warn_unused_result func advancedBy(_ n: Self.Distance, limit limit: Self) -> Self // duped from ForwardIndexType?
-    //@warn_unused_result func distanceTo(_ other: Self) -> Self.Distance                // duped from ForwardIndexType?
+	//@warn_unused_result func advancedBy(_ n: Self.Distance) -> Self					// duped from ForwardIndexType?
+	//@warn_unused_result func advancedBy(_ n: Self.Distance, limit limit: Self) -> Self // duped from ForwardIndexType?
+	//@warn_unused_result func distanceTo(_ other: Self) -> Self.Distance				// duped from ForwardIndexType?
 }
 
 // workaround for error E36: Interface type expected, found "IntegerLiteralConvertible<T>!"
 public protocol IntegerType : RandomAccessIndexType {
-    // no members
+	// no members
 }
 
