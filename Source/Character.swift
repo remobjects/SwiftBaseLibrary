79893905a91f74e66017b4d9385f34a5ef0053f3
marc hoffman <mh@remobjects.com>
2016-09-18 11:21:23 -0400

    Character: workaround for 76108 (fine too keep as final code)

--- Source/Character.swift
+++ Source/Character.swift
@@ -11,20 +11,19 @@
 			return UInt32(nativeStringValue[0]).toHexString(length: 4)
 		} else if length(nativeStringValue) > 1 {
 			var result = ""
-			//var currentSurrogate: Int32?
-			var currentSurrogate: Char? = nil
+			var currentSurrogate: Char = "\0"
 			for i in 0 ..< length(nativeStringValue) {
 
 				let c = nativeStringValue[i]
 				let c2 = UInt32(c)
 				var newChar: String? = nil
 				if c2 <= 0x0D7FF || c2 > 0x00E000 {
-					if currentSurrogate != nil {
+					if currentSurrogate != "\0" {
 						throw Exception("Invalid surrogate pair at index \(i)")
 					}
 					newChar = UInt32(nativeStringValue[i]).toHexString(length: 4)
 				} else if c2 >= 0x00D800 && c2 <= 0x00DBFF {
-					if currentSurrogate != nil {
+					if currentSurrogate != "\0" {
 						throw Exception("Invalid surrogate pair at index \(i)")
 					}
 					currentSurrogate = c
@@ -34,7 +33,7 @@
 						code += (surrogate & 0x03FF) << 10;
 						code += (c & 0x03FF);
 						newChar = UInt32(code).toHexString(length: 6)
-						currentSurrogate = nil
+						currentSurrogate = "\0"
 					} else {
 						throw Exception("Invalid surrogate pair at index \(i)")
 					}
