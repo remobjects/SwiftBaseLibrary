a6de7d0c6b098796b24632f0b6961638e1a4c07a
marc hoffman <mh@remobjects.com>
2017-08-21 11:34:30 -0400

    Some CGRect & Co extensions

---
+++ Source/CG_Extensions.swift
@@ -0,0 +1,53 @@
+﻿#if COCOA
+import CoreGraphics
+
+public extension CGPoint {
+
+	init() {
+		return CoreGraphics.CGPointZero
+	}
+
+	init(x: CGFloat, y: CGFloat) {
+		return CGPointMake(x, y)
+	}
+
+	var zero: CGPoint { return CoreGraphics.CGPointZero }
+}
+
+public extension CGSize {
+
+	init() {
+		return CoreGraphics.CGSizeZero
+	}
+
+	init(width: CGFloat, height: CGFloat) {
+		return CGSizeMake(width, height)
+	}
+
+	var zero: CGSize { return CoreGraphics.CGSizeZero }
+}
+
+public extension CGRect {
+
+	init() {
+		return CoreGraphics.CGRectZero
+	}
+
+	init(origin: CGPoint, size: CGSize) {
+		return CGRectMake(origin.x, origin.y, size.width, size.height)
+	}
+
+	init(x: CGFloat, y: CGFloat, width: CGFloat, height: CGFloat) {
+		return CGRectMake(x, y, width, height)
+	}
+
+	var x: CGFloat { return origin.x }
+	var y: CGFloat { return origin.y }
+	var width: CGFloat { return size.width }
+	var height: CGFloat { return size.height }
+
+	var zero: CGRect { return CoreGraphics.CGRectZero }
+	var null: CGRect { return CoreGraphics.CGRectNull }
+	var infinite: CGRect { return CoreGraphics.CGRectInfinite }
+}
+#endif
---
+++ Source/Foundation_Extension.swift
@@ -0,0 +1,7 @@
+﻿#if COCOA
+import Foundation
+
+@inline(always) func NSLocalizedString(_ key: String, comment: String) -> String {
+	return NSBundle.mainBundle.localizedStringForKey(key, value: "", table: nil)
+}
+#endif
