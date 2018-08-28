ec54a92b2174291c16b106bd2dbe56859bbc97b7
Carlo Kok <ck@remobjects.com>
2018-08-28 12:27:31 +0200

    80343: Generics, Nullables and Value Typed (Echoes)

--- Source/Sequence_Functions.swift
+++ Source/Sequence_Functions.swift
@@ -82,7 +82,7 @@ public func split(_ elements: String, separatorChar separator: Char) -> [String]
 public func sequence<T>(first: T, next: (T) -> T?) -> ISequence<T> {
 	var nextResult: T? = first
 	while nextResult != nil {
-		__yield nextResult
+		__yield nextResult?
 		nextResult = next(nextResult!)
 	}
 }
