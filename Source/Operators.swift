b85e068b25d4591ad971a1833e42c5d29c593190
marc hoffman <mh@remobjects.com>
2018-11-07 12:54:55 -0400

    80534: SE-0217 Introducing the !! "Unwrap or Die" operator

---
+++ Source/Operators.swift
@@ -0,0 +1,13 @@
+﻿public postfix func !! <T> (b: T?) -> T {
+	if let x = b {
+		return x
+	}
+	#if JAVA
+	__throw NullPointerException("Cannot force-unwrap reference")
+	#elseif CLR || ISLAND
+	__throw NullReferenceException("Cannot force-unwrap reference")
+	#elseif COCOA
+	__throw NSException(name: "Cannot force-unwrap reference", reason: "Cannot force-unwrap reference", userInfo: nil)
+	#endif
+
+}
